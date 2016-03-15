449d264ed1859a13322135728ddff832d71d3676
Ciprian Roman <ciprian@dreamitget.it>
2016-03-15 22:05:06 +0000

    Removed other settings not needed for now.

--- brew.sh
+++ brew.sh
@@ -3,10 +3,10 @@
 # Install command-line tools using Homebrew.
 
 # Ask for the administrator password upfront.
-sudo -v
+#sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -47,8 +47,8 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install other useful binaries.
-brew install git-lfs
-brew install imagemagick --with-webp
+#brew install git-lfs
+#brew install imagemagick --with-webp
 
 # Remove outdated versions from the cellar.
 brew cleanup
